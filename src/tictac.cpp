48da86c7d4a2182a2c431b3c5afa1fa17a42f3e0
giulia <giuliamferreira01@gmail.com>
2025-06-07 15:57:54 -0300

    TDD: verifica vitoria pra X e O -passou

--- src/tictac.cpp
+++ src/tictac.cpp
@@ -1,16 +1,23 @@
-#include "tictac.hpp"
-
 int verificaVencedor(int tabuleiro[3][3]) {
-    // Verifica se há algum espaço vazio (0)
+    // Verifica vitória em linhas
+    for (int i = 0; i < 3; ++i) {
+        if (tabuleiro[i][0] != 0 &&
+            tabuleiro[i][0] == tabuleiro[i][1] &&
+            tabuleiro[i][1] == tabuleiro[i][2]) {
+            return tabuleiro[i][0];
+            }
+    }
+
+    // Verifica se o tabuleiro está cheio
     bool temEspacoVazio = false;
     for (int i = 0; i < 3 && !temEspacoVazio; ++i)
         for (int j = 0; j < 3; ++j)
             if (tabuleiro[i][j] == 0)
                 temEspacoVazio = true;
 
-    // Se não há espaços vazios e nenhum venceu, então é empate
+    // Empate se não há espaços vazios
     if (!temEspacoVazio)
         return 0;
 
-    return -1; // padrão do primeiro teste
+    return -1;
 }
