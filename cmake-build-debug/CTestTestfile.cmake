4c56acec08ca407c296dd9d2be79e0b4c4e3b15b
giulia <giuliamferreira01@gmail.com>
2025-06-07 17:12:26 -0300

    Adapta CMakeList: sem flags o suficiente

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/giuli/Documents/TP2/tic_tac_toe
+# Build directory: C:/Users/giuli/Documents/TP2/tic_tac_toe/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TicTacTests "C:/Users/giuli/Documents/TP2/tic_tac_toe/cmake-build-debug/test_tictac.exe")
+set_tests_properties(TicTacTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/giuli/Documents/TP2/tic_tac_toe/CMakeLists.txt;19;add_test;C:/Users/giuli/Documents/TP2/tic_tac_toe/CMakeLists.txt;0;")
+subdirs("googletest")
---
+++ cmake-build-debug/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/giuli/Documents/TP2/tic_tac_toe/googletest/googletest
+# Build directory: C:/Users/giuli/Documents/TP2/tic_tac_toe/cmake-build-debug/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
